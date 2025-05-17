9a8111e4b15bed78e8fb2230ebea9bf6d6c7c819
SathwikPedapati <161137317+sathwikpedapati@users.noreply.github.com>
2025-05-18 00:44:04 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,9 @@ require("./Models/db.js");
 const PORT=process.env.PORT||8080;
 app.use(bodyParser.json());
 app.use(cors());
+app.get("/pong",(req,res)=>{
+    res.send("pong");
+})
 app.use("/auth",AuthRouter );
 app.listen(PORT,()=>{
     console.log(`server is running at ${PORT}`)
